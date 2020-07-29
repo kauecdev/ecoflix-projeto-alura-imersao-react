ad2f55947488600d97d48df85cd446bf8f322b31
kauecdev <kaue_cavalcante.cnt@outlook.com>
2020-07-29 08:35:30 -0300

    Segundo dia

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -8,8 +8,6 @@ const Main = styled.main`
   color: var(--white);
   flex: 1;
   padding-top: 50px;
-  padding-left: 5%;
-  padding-right: 5%;
 `;
 
 function PageDefault({ children }) {
--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PageDefault from '../../components/PageDefault';
+
+import ImgCinema from '../../assets/img/sala_cinema2.jpg';
+import './notFound.css';
 
 function notFound() {
   return (
-    <div>
-      Página não encontrada - ERRO 404
-    </div>
+    <PageDefault>
+      <div className="title-404">
+        <h1>A página que você procurou parece não existir...</h1>
+        <h3>Tem certeza de estar no endereço correto? Experimente recomeçar voltando para a home clicando na logo.</h3>
+      </div>
+      <img className="img-sala-cinema" src={ImgCinema} alt="Página não encontrada" />
+    </PageDefault>
   )
 }
 
